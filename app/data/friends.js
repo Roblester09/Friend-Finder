6943156a509181a0c47edbe2882d7fad70936c5e
Robert Lester <Lesters.robert@gmail.com>
2017-04-25 22:14:42 -0400

    created the server, routes, and html pages

---
+++ app/data/friends.js
@@ -0,0 +1,21 @@
+
+var friendsArray = [
+    {
+        name: "Robert",
+        photo: "",
+        scores: [
+            4,
+            5,
+            5,
+            3,
+            2,
+            1,
+            3,
+            3,
+            4,
+            5
+        ]
+    }
+];
+
+module.exports = friendsArray;
